a5f74bcbde329c37d9da2beaae8b0913f347a33c
richHong <rich.hong@gmail.com>
2016-04-06 15:50:10 -0700

    Passed first test

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,26 +1,38 @@
 var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+var http = require('http-request')
 // require more modules/folders here!
 var headers = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
   "access-control-max-age": 10, // Seconds.
-  "Content-Type": "application/json",
+  "Content-Type": "text/html",
 };
 
 exports.handleRequest = function (req, res) {
-  // console.log('request: ',req);
   if (req.method === 'GET') {
-    fs.readFile('/public/loading.html', function (err, data) {
-    console.log('our data: ',data);     
-    res.writehead(200, headers);
-    res.write(data);
-    res.end(archive.paths.list);
-    });    
+    // console.log('res:',res)
+    fs.readFile('web/public/index.html', function (err, data){
+      console.log('data:',data);
+      res.writeHead(200, headers);
+      res.write(data);
+      res.end(archive.paths.list);
+    });
+    // http.get({
+    //   'url':'http://localhost/public',
+    //   'proxy': {
+    //     host: 'localhost',
+    //     port: 8080
+    //   }
+    // }, 'index.html', function (err, res) {
+    //   res.writeHead(200, headers);
+    //   res.write(response);
+    //   res.end(archive.paths.list);
+    //   })
   } else if (req.method === 'POST') {
-    res.writehead(200, headers);
+    res.writeHead(200, headers);
     res.end(archive.paths.list);
   }
 };
