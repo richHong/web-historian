d7634eefd1697a6b765c9669ece79b641e24c20e
richHong <rich.hong@gmail.com>
2016-04-07 12:21:43 -0700

    Getting closer. url still a problem but data chunk acquired

--- web/request-handler.js
+++ web/request-handler.js
@@ -10,38 +10,75 @@ var headers = {
   "access-control-max-age": 10, // Seconds.
   "Content-Type": "text/html",
 };
+var router = [{route:'/', location:'web/public/index.html'}, {route:'/www.google.com', location:'archives/sites/www.google.com'}];
 
 exports.handleRequest = function (req, res) {
   if (req.method === 'GET') {
-    if(req.url === '/'){
-      fs.readFile('web/public/index.html', function (err, data){
-        res.writeHead(200, headers);
-        res.write(data);
-        res.end(archive.paths.list);
-      });
-    } else if (req.url === '/www.google.com') {  // TODO find actual var to pass isntead of 'google.com'.
-      fs.readFile('archives/sites/www.google.com', function (err, data){
-        res.writeHead(200, {"Content-Type": "text/plain"});
-        res.write(data);
-        res.end(archive.paths.list);
-      });
-    } else {
-        res.writeHead(404, {"Content-Type": "text/plain"});
-        res.end();
-    }
-  } else if (req.method === 'POST') {
-    console.log(req.url);
-    fs.writeFile(archive.paths.list, 'www.example.com'+'\n', function(err) {
-      if (err) {
-        console.log(err);
-        res.end();
-      } else {
-        console.log('Success');
-        res.writeHead(302, {"Content-Type": "text/plain"});
-        res.end();
+    var found = false;
+    router.forEach(function (page){
+      if (req.url === page.route){
+        found = true;
+        fs.readFile(page.location, function (err, data){
+          res.writeHead(200, headers);
+          res.write(data);
+          res.end(archive.paths.list);
+        })
       }
+    })
+    if (!found){
+      res.writeHead(404, {"Content-Type": "text/plain"});
+      res.end();
+    } 
+    // if(req.url === '/'){
+    //   fs.readFile('web/public/index.html', function (err, data){
+    //     res.writeHead(200, headers);
+    //     res.write(data);
+    //     res.end(archive.paths.list);
+    //   });
+    // } else if (req.url === '/www.google.com') {  // TODO find actual var to pass isntead of 'google.com'.
+    //   fs.readFile('archives/sites/www.google.com', function (err, data){
+    //     res.writeHead(200, {"Content-Type": "text/plain"});
+    //     res.write(data);
+    //     res.end(archive.paths.list);
+    //   });
+    // } else {
+    //     res.writeHead(404, {"Content-Type": "text/plain"});
+    //     res.end();
+    // }
+  } else if (req.method === 'POST') {
+    var body = '';
+    var parsed; 
+
+    req.addListener('data', function(chunk){
+      console.log('got a chunk');
+      body += chunk;
     });
-    res.writeHead(302, headers);
-    res.end();
+
+    req.addListener('error', function(error){
+      console.error('got a error', error);
+      next(err);
+    });
+
+    req.addListener('end', function(chunk){
+      console.log('ended');
+      if (chunk) {
+        body += chunk;
+      }
+
+      parsed = JSON.parse(body);
+
+      fs.appendFile(archive.paths.list, 'string' +'\n', function(err) {
+        console.log('parsed in fs.writeFile', parsed);
+        if (err) {
+          console.log(err);
+          res.end();
+        } else {
+          console.log('Success');
+          res.writeHead(302, {"Content-Type": "text/plain"});
+          res.end();
+        }
+      });
+      
+    });     
   }
 };
